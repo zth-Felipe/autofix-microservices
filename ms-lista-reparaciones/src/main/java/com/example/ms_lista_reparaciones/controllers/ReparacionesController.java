86cae878e10096a4a41b83c7ae5a664ee6fd7dbb
zth-Felipe <92702735+zth-Felipe@users.noreply.github.com>
2024-08-12 10:39:45 -0400

    Add files via upload

--- ms-lista-reparaciones/src/main/java/com/example/ms_lista_reparaciones/controllers/ReparacionesController.java
+++ ms-lista-reparaciones/src/main/java/com/example/ms_lista_reparaciones/controllers/ReparacionesController.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/reparaciones")
-@CrossOrigin("*")
 public class ReparacionesController {
     @Autowired
     ReparacionesService reparacionesService;
